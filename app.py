0e6aec6038122550b7539fbb00ea23124138f8f5
tejaskadam009 <tejaskadam0986@gmail.com>
2026-01-31 03:21:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("🩺 AI Health Guidance (Demo)")
 st.caption("⚠️ For awareness only. Not a diagnosis tool.")
 
 HF_TOKEN = st.secrets.get("HF_TOKEN", "")
-TEXT_MODEL = "google/flan-t5-large"
+TEXT_MODEL = "google/flan-t5-base"
 
 demo_cases = {
     "Case 1: Fever + Body pain": "I have fever, body pain, weakness since 2 days.",
